e686835de9861e67f71f8c56bf467ea12aa419a4
gamegine <gamegine@protonmail.com>
2025-05-28 11:15:45 +0200

    init

---
+++ VSUnitTest/VSUnitTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace VSUnitTest
+{
+	TEST_CLASS(VSUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
